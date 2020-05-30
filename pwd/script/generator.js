2fbebf29b4cbd584ee525b467730520a7ad5b423
nobus-1967 <avshcherbina@gmail.com>
2020-05-30 11:37:30 +0300

    Fixed some indents

--- pwd/script/generator.js
+++ pwd/script/generator.js
@@ -1,20 +1,21 @@
-const ABC = "AaBbCcDdEeFfGgHhIiJiKkLlMmNnJjPpQqRrSsTtUuVvWwXxYyZz";
-const NUMBERS = "0123456789";
-const SIGNS = "-_";
-const LETTERS = ABC + NUMBERS;
-const SYMBOLS = ABC + NUMBERS + SIGNS;
-let password = "";
+const ABC = 'AaBbCcDdEeFfGgHhIiJiKkLlMmNnJjPpQqRrSsTtUuVvWwXxYyZz'
+const NUMBERS = '0123456789'
+const SIGNS = '-_'
+const LETTERS = ABC + NUMBERS
+const SYMBOLS = ABC + NUMBERS + SIGNS
+let password = ''
 
 function getPassword(FIRST, SECOND, THIRD) {
-    password += FIRST[Math.floor(Math.random() * FIRST.length)];
-    for (let i = 0; i < 6; ++i) {
-        password += SECOND[Math.floor(Math.random() * SECOND.length)];
-    }
-    password += THIRD[Math.floor(Math.random() * THIRD.length)];
-    return password;
+  password += FIRST[Math.floor(Math.random() * FIRST.length)]
+  for (let i = 0; i < 6; ++i) {
+    password += SECOND[Math.floor(Math.random() * SECOND.length)]
+  }
+  password += THIRD[Math.floor(Math.random() * THIRD.length)]
+  return password
 }
-getPassword(ABC, SYMBOLS, LETTERS);
+
+getPassword(ABC, SYMBOLS, LETTERS)
 
 function alertPassword() {
-    alert("Password: " + password);
+  alert('Password: ' + password)
 }
